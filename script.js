8da24da706e335d80e09e23a290214c71346f6d0
Skay <dobrin_k1@abv.bg>
2023-01-19 17:01:06 +0200

    done

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("imTruing").style.opacity = "1";
+document.getElementById("portfoiloTextPog").style.opacity = "1";
+document.getElementById("leaddevserver").style.opacity = "1";
+document.getElementById("about-me").style.opacity = "1";
+document.getElementById("suppressed").style.opacity = "1";
